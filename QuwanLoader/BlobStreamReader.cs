1d0f46f8fb7b04ea91e3d211f0dcda72aba34e93
Yue Fei <yufei@microsoft.com>
2022-10-22 22:22:58 +0800

    Fix blob exception

--- QuwanLoader/BlobStreamReader.cs
+++ QuwanLoader/BlobStreamReader.cs
@@ -71,29 +71,36 @@ public class BlobStreamReader
     {
         _logger.LogInformation($"Read blob content from {blobUrl}");
         cancellationToken.ThrowIfCancellationRequested();
-
-        var blobClient = new BlobClient(new Uri(blobUrl), _tokenCredential);
-        using var stream = await blobClient.OpenReadAsync(0, bufferSize: 40960, cancellationToken: cancellationToken);
-        using var streamReader = new StreamReader(stream);
-        string line;
         int index = 1;
-        do
-        {
-            line = await streamReader.ReadLineAsync();
-            if (await writer.WaitToWriteAsync())
-            {
-                MatchCollection matches = Regex.Matches(line, "[\"resourceType\"]:\"([A-Za-z]+)\",\"id\":\"([a-z0-9-]+)\"");
-                var item = new ResourceItem { Resource = line, BlobName = blobUrl, Index = index, ResourceType = matches[0].Groups[1].Value, Id = matches[0].Groups[2].Value };
-                await writer.WriteAsync(item);
-            }
-            index++;
 
-            if (index % 500 == 0)
+        try
+        {
+            var blobClient = new BlobClient(new Uri(blobUrl), _tokenCredential);
+            using var stream = await blobClient.OpenReadAsync(0, bufferSize: 40960, cancellationToken: cancellationToken);
+            using var streamReader = new StreamReader(stream);
+            string line;
+            do
             {
-                _logger.LogInformation($"Load {index} resources from {blobUrl}");
+                line = await streamReader.ReadLineAsync();
+                if (await writer.WaitToWriteAsync())
+                {
+                    MatchCollection matches = Regex.Matches(line, "[\"resourceType\"]:\"([A-Za-z]+)\",\"id\":\"([a-z0-9-]+)\"");
+                    var item = new ResourceItem { Resource = line, BlobName = blobUrl, Index = index, ResourceType = matches[0].Groups[1].Value, Id = matches[0].Groups[2].Value };
+                    await writer.WriteAsync(item);
+                }
+                index++;
+
+                if (index % 500 == 0)
+                {
+                    _logger.LogInformation($"Load {index} resources from {blobUrl}");
+                }
             }
+            while (line != null);
         }
-        while (line != null);
+        catch (Exception ex)
+        {
+            _logger.LogError($"Read blob {blobUrl} failed: {ex}");
+        }
 
         _logger.LogInformation($"Completed reading {index - 1} resources from {blobUrl}");
     }
