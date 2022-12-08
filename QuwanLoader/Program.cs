d0295072363c11a1f6dc1e0773a288f1f17bf191
QuanWanxx <68055742+QuanWanxx@users.noreply.github.com>
2022-12-08 10:18:07 +0800

    Host To Upload FHIR Data (#2)
    * Upload from blobs
    
    * Update readme
    
    * fix expire duration bug
    
    * Add blob generator
    
    * Add client secret credential config
    
    * change config
    
    * Refine token aquire and error handling
    
    * Ensure token acquired before starting
    
    * Add file telementry
    
    * Fix blob exception
    
    * Update blob error handling
    
    * Update logging
    
    * Fix sending twice exception
    
    * update progress
    
    * Add 200g and 300g list
    
    * Commit blob list
    
    * Update uploader
    
    * fix task canceledexception
    
    Co-authored-by: Yue Fei <yufei@microsoft.com>
    Co-authored-by: Ubuntu <yufei@yufeivmtest.opbp4mgcdw1enhnmyhxnj2keqd.bx.internal.cloudapp.net>

---
+++ QuwanLoader/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.ApplicationInsights.WorkerService;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using QuwanLoader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace FhirLoader.QuwanLoader
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            using IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureHostConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    configHost.AddJsonFile("appsettings.json", optional: true);
+                    configHost.AddEnvironmentVariables(prefix: "PREFIX_");
+                    configHost.AddCommandLine(args);
+                })
+               .ConfigureServices((context, services) =>
+               {
+                   var configurationRoot = context.Configuration;
+                   services.Configure<UploadConfiguration>(configurationRoot);
+                   services.AddHttpClient();
+                   services
+                    .AddSingleton<FhirAccessTokenProvider>()
+                    .AddSingleton<FhirUploader>()
+                    .AddSingleton<BlobStreamReader>()
+                    .AddHostedService<FhirUploadService>()
+                    .AddApplicationInsightsTelemetryWorkerService(new ApplicationInsightsServiceOptions {  EnableDependencyTrackingTelemetryModule = false} );
+    
+                   var logger = services.BuildServiceProvider().GetRequiredService<ILogger<Program>>();
+                   logger.LogInformation("Logging setup.");
+               }).Build();
+
+            // Application code should start here.
+
+            await host.RunAsync();
+
+        }
+    }
+}
